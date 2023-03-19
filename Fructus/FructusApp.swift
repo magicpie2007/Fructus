e9e2819e79e3a5b0ba6732c75fcf5021ffabf41a
magcipie2007 <magicpie2007@gmail.com>
2023-03-19 11:27:57 +0900

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by hiroyuki shoji on 2023/03/19.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
